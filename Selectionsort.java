b8b68941c7d96bb15f0fd0f51ffc35cbad9231e1
Pranshu Sati <95905172+Pranshu1sati@users.noreply.github.com>
2023-01-07 22:16:47 +0530

    Add files via upload

---
+++ Selectionsort.java
@@ -0,0 +1,30 @@
+public class Selectionsort {
+    public static void printArray(int arr[])
+	{
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+            System.out.println();
+	}
+    public static void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {
+        int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String args[])
+	{
+		int arr[] = { 5, 1, 4, 2, 8 };
+		sort(arr);
+		System.out.println("Sorted array");
+		printArray(arr);
+	}
+}
